22c45960a478dcce958b24170ebb6696d1005d05
sameli <sohrab5888@gmail.com>
2015-11-09 18:35:33 -0400

    Add Mono Compiler

--- MonoCompiler.aspx.cs
+++ MonoCompiler.aspx.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.Collections;
+using Mono.CSharp;
 
 namespace OnlineCompiler
 {
@@ -28,9 +33,27 @@ namespace OnlineCompiler
 		}
 
 		public string compileCode(string code){
+			/*
 			Object result = new Object (); //TODO
 			string jsontmp = toJSON (result);
 			return jsontmp;
+			*/
+
+			CompilerOutput compilerOutput = new CompilerOutput ();
+			/*
+		var compilerContext = new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter());
+
+		var evaluator = new Evaluator(compilerContext);
+		*/
+			var reportWriter = new StringWriter();
+			var settings = new CompilerSettings();
+			var printer = new ConsoleReportPrinter(reportWriter);
+
+			var compilerContext = new CompilerContext (settings, printer);
+			var reports = new Report(compilerContext, printer);
+			var evaluator = new Evaluator(compilerContext);
+			//evaluator.Run ("MainClass m1 = new MainClass(); m1.Main();");
+			//evaluator.Compile (mainCode);
 		}
 	}
 }
